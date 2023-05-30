52ba0d12b302d83ad07dbcebd57d0f0aa6f1cb42
Ausra311 <evacaldin@gmail.com>
2023-05-30 17:54:47 +0900

    fit: parser de phrases, plusieurs possibilites de reconnaissance

--- src/noyau_fonctionnel/language/text_analysis/text_analyse.py
+++ src/noyau_fonctionnel/language/text_analysis/text_analyse.py
@@ -1,13 +1,15 @@
-# to split sentence
 import spacy
-# to have the list of stop words
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from sep_token import sep_token
+import numpy as np
 
 class parse():
 
     def __init__(self):
         self.stop_word = set(stopwords.words('french')) # french stop words
-
+        self.snowball_stemmer = SnowballStemmer(language='french')
+    
     def return_token_word(self, sentence):
         # split sentence to list of token (1 token = 1 word)
         doc = nlp(sentence)
@@ -19,16 +21,46 @@ class parse():
         for token in tokens:
             if token not in self.stop_word:
                 clean_tok.append(token)
-
         return clean_tok
+    
+    def return_stem(self, sentence):
+        doc = nlp(sentence)
+        return [self.snowball_stemmer.stem(X.text) for X in doc]
 
     def return_token_sentence(self, sentence):
         # 1 token = 1 sentence
+        tok = sep_token(sentence)
+        return tok.sentence_token()
+    
+    def return_NER(self, sentence):
+        # Named Entity Recognise
         doc = nlp(sentence)
-        return [X.text for X in doc.sents]
+        return [(X.text, X.label_) for X in doc.ents]
+    
+    def return_POS(self, sentence):
+        # Part Of Speech
+        doc = nlp(sentence)
+        return [(X, X.pos_) for X in doc]
+
+    def return_word_embedding(self, sentence):
+        # value of tokens
+        doc = nlp(sentence)
+        return [(X.vector) for X in doc]
+    
+    def return_mean_embedding(self, sentence):
+        # value of sentence
+        doc = nlp(sentence)
+        return np.mean([(X.vector) for X in doc], axis=0)
+    
+    def compare_sentences(self, sentence1, sentence2, dist):
+        dist = self.return_mean_embedding(sentence1)-self.return_mean_embedding(sentence2)
+        if(dist > -dist and dist < dist):
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-    test = "oui, j'ai passé une bonne nuit, merci... Et toi ?"
+    test = "oui, MeiChan, j'ai passé une bonne nuit, merci... Et toi ?"
     nlp = spacy.load("fr_core_news_sm")
 
     stopW = set(stopwords.words('french'))
@@ -45,4 +77,12 @@ if __name__ == '__main__':
     clean_tok1 = par.clean_tokens_word(tok1)
     #clean_tok2 = par.clean_tokens_word(tok2)
 
-    print(sep_sentence[0])
+    stem_tok0 = par.return_stem(sep_sentence[0])
+    stem_tok1 = par.return_stem(sep_sentence[1])
+    #stem_tok2 = par.return_stem(sep_sentence[2])
+
+    ner = par.return_NER(test)
+
+    pos = par.return_POS(test)
+
+    print(pos)
