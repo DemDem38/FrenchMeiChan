3742040586a392b3df24b54f241e7728916b13a9
Ausra311 <evacaldin@gmail.com>
2023-06-02 12:33:28 +0900

    fit: classes pour definir des personnes

---
+++ src/noyau_fonctionnel/authentication/account.py
@@ -0,0 +1,40 @@
+
+class account():
+    def __init__(self, last_name, first_name, birthday, email, phone_number, contact_last_name, contact_first_name, contact_email, contact_phone_number):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthday = birthday
+        self.email = email
+        self.phone_number = phone_number
+        self.contact_last_name = contact_last_name
+        self.contact_first_name = contact_first_name
+        self.contact_email = contact_email
+        self.contact_phone_number = contact_phone_number
+
+    def get_last_name(self):
+        return self.last_name
+    
+    def get_last_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.birthday
+    
+    def get_last_name(self):
+        return self.email
+    
+    def get_last_name(self):
+        return self.phone_number
+    
+    def get_last_name(self):
+        return self.contact_last_name
+    
+    def get_last_name(self):
+        return self.contact_first_name
+    
+    def get_last_name(self):
+        return self.contact_email
+    
+    def get_last_name(self):
+        return self.contact_phone_number
+    
---
+++ src/noyau_fonctionnel/authentication/person.py
@@ -0,0 +1,7 @@
+class person():
+    def __init__(self, last_name="X", first_name="x", birthday="00/00/0000", phone="0000000000", email="x@x.x"):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthday = birthday
+        self.phone = phone
+        self.email = email
