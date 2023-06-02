26cb470b413b2805d838d33f6c317afb9773678c
Ausra311 <evacaldin@gmail.com>
2023-06-02 14:56:04 +0900

    fit: amelioration de creation de compte, ajout de contacts

--- src/noyau_fonctionnel/authentication/account.py
+++ src/noyau_fonctionnel/authentication/account.py
@@ -1,40 +1,30 @@
+from person import person
+import warnings
 
 class account():
-    def __init__(self, last_name, first_name, birthday, email, phone_number, contact_last_name, contact_first_name, contact_email, contact_phone_number):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.birthday = birthday
-        self.email = email
-        self.phone_number = phone_number
-        self.contact_last_name = contact_last_name
-        self.contact_first_name = contact_first_name
-        self.contact_email = contact_email
-        self.contact_phone_number = contact_phone_number
+    def __init__(self, user, first_contact, other_contacts=[]):
+        self.preson = user
+        self.first_contact = first_contact
+        if other_contacts != []:
+            nb_contacts = len(other_contacts)
+            if nb_contacts > 4:
+                nb_contacts = 4
+                warnings.warn("Le nombre maximum de contact est de 5 personnes,\
+                              il y en a trop, seulement les 5 premiers seront enregistres")
+            for i in range (nb_contacts):
+                self["contact"+str(i+2)] = other_contacts[i]
+            self.nb_contacts = nb_contacts+1
+        else:
+            self.nb_contacts = 1
 
-    def get_last_name(self):
-        return self.last_name
-    
-    def get_last_name(self):
-        return self.first_name
-    
-    def get_last_name(self):
-        return self.birthday
-    
-    def get_last_name(self):
-        return self.email
-    
-    def get_last_name(self):
-        return self.phone_number
-    
-    def get_last_name(self):
-        return self.contact_last_name
-    
-    def get_last_name(self):
-        return self.contact_first_name
-    
-    def get_last_name(self):
-        return self.contact_email
-    
-    def get_last_name(self):
-        return self.contact_phone_number
-    
+    def get_nb_contacts(self):
+        return self.nb_contacts
+
+    def set_new_contact(self, contact):
+        if self.nb_contacts == 5:
+            warnings.warn("Le nombre maximum de contact est atteint, veuillez en supprimer pour en ajouter un nouveau")
+        else:
+            self["contact"+str(self.nb_contacts)] = contact
+            self.nb_contacts += 1
+
+         # list of persons to contact
--- src/noyau_fonctionnel/authentication/person.py
+++ src/noyau_fonctionnel/authentication/person.py
@@ -5,3 +5,28 @@ class person():
         self.birthday = birthday
         self.phone = phone
         self.email = email
+
+    def get_last_name(self):
+        return self.last_name
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+
+    def get_first_name(self):
+        return self.first_name
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+
+    def get_birthday(self):
+        return self.birthday
+    def set_birthday(self, birthday):
+        self.birthday = birthday
+
+    def get_phone(self):
+        return self.phone
+    def set_phone(self, phone):
+        self.phone = phone
+
+    def get_email(self):
+        return self.email
+    def set_email(self, email):
+        self.email = email
