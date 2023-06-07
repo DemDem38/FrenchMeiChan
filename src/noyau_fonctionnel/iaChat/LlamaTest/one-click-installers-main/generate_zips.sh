7e052c6657ef4f71cabfc872cda8086c9f54cd82
DemDem <adam.larabi41@gmail.com>
2023-06-07 19:05:40 +0900

    :refractor: modification et suppression de fichier

--- src/noyau_fonctionnel/iaChat/LlamaTest/one-click-installers-main/generate_zips.sh
+++
@@ -1,5 +0,0 @@
-mkdir oobabooga_{windows,linux,macos}
-for p in windows macos linux; do
-  cp {*$p*\.*,webui.py,INSTRUCTIONS.TXT} oobabooga_$p;
-  zip -r oobabooga_$p.zip oobabooga_$p;
-done
--- src/noyau_fonctionnel/iaChat/ia.py
+++ src/noyau_fonctionnel/iaChat/ia.py
@@ -3,11 +3,21 @@ from langchain import PromptTemplate, LLMChain
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-def get_text():
-    """
-    Get the user input text.
-    Returns:
-        (str): The text entered by the user
-    """
-    input_text = "Replace to take the input user"
-    return input_text
+
+
+template = """Question: {question}
+
+Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+llm = LlamaCpp(
+    model_path="./llama.cpp/models/7B/ggml-model-q4_0.bin", 
+    callback_manager=callback_manager, 
+    verbose=True
+)
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+text = "What would be a good company name for a company that makes colorful socks?"
+llm_chain.run(text)
