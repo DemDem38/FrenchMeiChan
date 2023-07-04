cc599657339dc464122c109a77166e6ff4513994
Ausra311 <evacaldin@gmail.com>
2023-07-04 17:03:09 +0900

    :feat: clqsse pour utiliser l'ia

--- src/noyau_fonctionnel/iaChat/ia.py
+++ src/noyau_fonctionnel/iaChat/ia.py
@@ -1,53 +1,26 @@
 from langchain import LLMChain
 from langchain.llms import GPT4All
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.agents import load_tools,initialize_agent,AgentType
 from langchain.memory import ConversationBufferWindowMemory
 from langchain import LLMChain, PromptTemplate
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.chains import SimpleSequentialChain
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-
-local_path = (
-    "C:\\Users\\frenchstudent\\FrenchMeiChan\\src\\noyau_fonctionnel\\iaChat\\models\\ggml-gpt4all-j-v1.3-groovy.bin"  # replace with your desired local file path
-)
-# Callbacks support token-wise streaming
-callbacks = [StreamingStdOutCallbackHandler()]
-llm = GPT4All(model=local_path, backend="gptj", callbacks=callbacks, verbose=False)
-
-template = """Assistant is a large language model with GPT4ALL.
-
-Assistant is designed to be able to assist with a wide range of tasks, 
-from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. 
-As a language model, Assistant is able to generate human-like text based on the input it receives, 
-allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-Assistant is constantly learning and improving, and its capabilities are constantly evolving.
-It is able to process and understand large amounts of text, and can use this knowledge to provide 
-accurate and informative responses to a wide range of questions. 
-Additionally, Assistant is able to generate its own text based on the input it receives, 
-allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-{history}
-Human: {human_input}
-Assistant:"""
-
-tools = load_tools(
-    ["arxiv"],
-)
-
-prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
-llm_chain = LLMChain(llm=llm, prompt=prompt,verbose=False,memory=ConversationBufferWindowMemory())
-
-#Agent use, with GPT4ALL, USE CASE 1
-agent = initialize_agent(tools, llm, verbose=False)
-
-while(True):
-    text=input("Human : ")
-    print("Mic : ")
-    #agent.run(input=text)
-    print(llm_chain.memory)
-    llm_chain.predict(human_input=text)
-    print("\n")
+from PyQt5.QtCore import pyqtSignal
+class agent():
+    def __init__(self,signal_listen,signal_send,model="C:\\Users\\frenchstudent\\FrenchMeiChan\\src\\noyau_fonctionnel\\iaChat\\models\\ggml-gpt4all-j-v1.3-groovy.bin"):
+        self.model=model
+        self.signal_listen=signal_listen
+        self.signal_send=signal_send
+        self.signal.connect(self.input_user)
+        llm = GPT4All(model=self.model, backend="gptj", verbose=False)
+        template = """
+        Your name is Mic
+        You are a assistante to help elder people. You speak with respect and try to make sure everythings is okay.
+        Elder people can be sick mentaly or physicly. You need to ask them if they are okay and if not tell them to call someone
+        You speak only english !
+        {history}
+        Human: {human_input}
+        Assistant:"""
+        prompt = PromptTemplate(input_variables=["history", "human_input"],template=template)
+        self.llm_chain = LLMChain(llm=llm, prompt=prompt,memory=ConversationBufferWindowMemory())
+    def input_user(self,text):
+        reponse= self.llm_chain.predict(human_input=text)
+        self.signal_send.emit(reponse)
 
